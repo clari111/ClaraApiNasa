169f2f3bf884243f10cd37ec6f3e07acbd234f50
claradehuevo <136344430+clari111@users.noreply.github.com>
2025-09-13 18:19:56 -0300

    ultimo

--- apod.js
+++ apod.js
@@ -57,4 +57,79 @@ fetch("https://api.nasa.gov/planetary/apod?api_key=4jmR71o3hrbJsbrjwZmqU2l3mUCEW
 
   });
 
+  let key = "4jmR71o3hrbJsbrjwZmqU2l3mUCEW98eglp9T1el"
+
+
+  let imagenDelDia = document.querySelector('#imagenDelDia')
+let boton = document.querySelector('#boton')
+let titulo = document.querySelector('h1')
+
+
+boton.onclick = function () {
+  
+  fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
+    .then(res => res.json())
+    .then(fotoDelDia => {
+      
+      imagenDelDia.src = fotoDelDia.hdurl
+      titulo.textContent = fotoDelDia.title
+    })
+}
+
+
+
+
+let imagenFecha = document.querySelector('#imagenFecha')
+
+// Guardamos el botón correspondiente
+let botonFecha = document.querySelector('#botonFecha')
+
+// Guardamos el input date
+let fechaUsuario = document.querySelector('#fecha')
+
+botonFecha.onclick = function () {
+  console.log(fechaUsuario) // Puedes quitar este log si no lo necesitas
+
+  // Solicitamos información a la API utilizando el valor seleccionado en el input
+  fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}&date=${fechaUsuario.value}`)
+    .then(res => res.json())
+    .then(fotoFecha => {
+      // Mostramos la imagen de la fecha seleccionada
+      imagenFecha.src = fotoFecha.hdurl
+    })
+}
+
+
+
+
+
+// Mostrar fotos aleatorias
+
+// Guardamos el contenedor de las imágenes a mostrar al azar
+let contenedorImagen = document.querySelector('.contenedor-imagen')
+
+// Guardamos el botón correspondiente
+let botonCantidad = document.querySelector('#botonCantidad')
+
+// Guardamos el input con la cantidad
+let cantidadUsuario = document.querySelector('#cantidad')
+
+botonCantidad.onclick = function () {
+  // Solicitamos información a la API
+  console.log(cantidadUsuario.value)
+
+  fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}&count=${cantidadUsuario.value}`)
+    .then(res => res.json())
+    .then(fotos => {
+      // Borramos el contenido previo, para que no se acumule con lo nuevo
+      contenedorImagen.innerHTML = ""
+
+      // Mostramos todas las imágenes recibidas
+      for (let i = 0; i < fotos.length; i++) {
+        contenedorImagen.innerHTML = `${contenedorImagen.innerHTML} <img src="${fotos[i].url}" alt="${fotos[i].title}">`
+      }
+    })
+}
+
+
 
